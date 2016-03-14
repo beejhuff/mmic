1b96b37d3bbc3bd4a0e2d8c597d098386b7e562d
eternnoir <eternnoir@gmail.com>
2016-03-14 10:38:45 +0800

    add getType for trello update.

--- mmic/payloads/trello/updateaction.go
+++ mmic/payloads/trello/updateaction.go
@@ -3,3 +3,16 @@ package trello
 type UpdateAction struct {
 	Action Action `json="action"`
 }
+
+func (ua *UpdateAction) GetType() string {
+	if _, found := ua.Action.Data["listBefore"]; found {
+		return "MoveCard"
+	}
+	if _, found := ua.Action.Data["old"]; found {
+		return "UpdateCard"
+	}
+	if ua.Action.Type == "createCard" {
+		return "CreateCard"
+	}
+	return ""
+}
